3afda35468608e02ced74ca4488d5c6f708e5133
Doppon <zaxpkuraudo7777@gmail.com>
2018-12-01 16:50:50 +0900

    テンプレートを指定

--- re_mysite/polls/views.py
+++ re_mysite/polls/views.py
@@ -5,8 +5,11 @@ from .models import Question
 
 def index(request):
     question_list = Question.objects.order_by('-pub_date')
-    output = ', '.join([q.question_text for q in question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
