7e4070397c685bbf18be78757dead8fe50da8911
Doppon <zaxpkuraudo7777@gmail.com>
2018-12-04 01:58:04 +0900

    掲示板の一覧ページと詳細ページを作成

--- re_mysite/bbs/urls.py
+++ re_mysite/bbs/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'bbs'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:topic_id>/', views.detail, name='detail'),
 ]
--- re_mysite/bbs/views.py
+++ re_mysite/bbs/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Topic, Post
 
 def index(request):
-    return HttpResponse("Hi This is BBS")
+    topic_list = Topic.objects.all()
+    context = {
+        'topic_list': topic_list,
+    }
+    return render(request, 'bbs/index.html', context)
+
+def detail(request, topic_id):
+    topic = get_object_or_404(Topic, pk=topic_id)
+    return render(request, 'bbs/detail.html', {'topic': topic})
