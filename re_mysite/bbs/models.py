b79c3809b5376fd75f167be0baa0402688c718eb
Doppon <zaxpkuraudo7777@gmail.com>
2018-12-03 21:34:23 +0900

    掲示板に必要なtopicモデルとpostモデルを作成

--- re_mysite/bbs/models.py
+++ re_mysite/bbs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    text = models.CharField(max_length=500)
