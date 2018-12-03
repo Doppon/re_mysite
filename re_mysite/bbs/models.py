957e4b62ec3f4f0ae768e467ea9ab088aa13a350
Doppon <zaxpkuraudo7777@gmail.com>
2018-12-03 21:46:26 +0900

    バグを修正

--- re_mysite/bbs/models.py
+++ re_mysite/bbs/models.py
@@ -10,4 +10,4 @@ class Post(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     text = models.CharField(max_length=500)
-    models.DateTimeField('登録日時', default=timezone.now)
+    created_at = models.DateTimeField('登録日時', default=timezone.now)
