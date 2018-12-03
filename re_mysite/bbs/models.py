bbec79a4949b19a612393133791e27dc63f35be3
Doppon <zaxpkuraudo7777@gmail.com>
2018-12-03 21:44:51 +0900

    掲示板のモデルに登録日時の項目を追加

--- re_mysite/bbs/models.py
+++ re_mysite/bbs/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 class Topic(models.Model):
     title = models.CharField(max_length=100)
+    created_at = models.DateTimeField('登録日時', default=timezone.now)
 
 
 class Post(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     text = models.CharField(max_length=500)
+    models.DateTimeField('登録日時', default=timezone.now)
