f6e8c5db29ce954c27ef21ed68b5692ae6e64ae9
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-05 18:27:47 +0900

    importの順番を整頓

--- re_mysite/polls/views.py
+++ re_mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question, Choice
 from django.urls import reverse
+
+from .models import Question, Choice
 
 def index(request):
     question_list = Question.objects.order_by('-pub_date')
