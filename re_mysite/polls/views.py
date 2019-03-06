8e7478e690e7f4cb8c1db34000a09c6fbc2a3aec
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-06 23:58:30 +0900

    Index部分をgenericを用いた書き方に更新

--- re_mysite/polls/views.py
+++ re_mysite/polls/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-def index(request):
-    question_list = Question.objects.order_by('-pub_date')
-    context = {
-        'question_list': question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
 def detail(request, question_id):
