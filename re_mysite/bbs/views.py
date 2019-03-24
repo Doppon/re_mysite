652fd870e5b18fa59f6c14a9c9ff8df32338b79e
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-24 22:39:32 +0900

    汎用型viewを適応するためにgenericをインポート

--- re_mysite/bbs/views.py
+++ re_mysite/bbs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views import generic
+
 from .models import Topic, Post
 
 def index(request):
