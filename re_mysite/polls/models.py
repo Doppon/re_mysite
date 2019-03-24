faa018848e2296ea7965a9826b58e19d44a7067f
Doppon <zaxpkuraudo7777@gmail.com>
2019-03-24 19:55:33 +0900

    改行を追加

--- re_mysite/polls/models.py
+++ re_mysite/polls/models.py
@@ -6,10 +6,12 @@ import datetime
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date-published')
+
     def was_published_recently(self):
         now = timezone.now()
         #    現時刻 -  1day                      <= pubdate       <= 現時刻
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
     def __str__(self):
         return self.question_text
 
@@ -19,5 +21,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
     def __str__(self):
         return self.choice_text
